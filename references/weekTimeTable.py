f87e864040e581c1d193ace3eec3ad3147db520a
Shashank Shetye Saudagar <shashanksaudagar@gmail.com>
2020-03-22 04:47:14 +0530

    Update weekTimeTable.py

--- references/weekTimeTable.py
+++ references/weekTimeTable.py
@@ -2,23 +2,52 @@
 import random
 random.seed()
 slots = 8
-subjects = ['a','b','c','d']
+halfslot = 4
+subjects = ['a','b','c','d','free']
+maxSubWeek = 4
+daysInWeek = 4
 def newEmptyDay():
     day = [None for x in range(slots)]
     slot = random.randint(0,slots-1)
     if slot == 3 or slot == 7:
-            return assignPrac()    
+            return newEmptyDay()    
+    day[slot] = 'prac'
+    day[slot + 1] = 'prac'
+    return day
+
+def newHalfDay():
+    day = [None for x in range(slots)]
+    for x in list(range(halfslot,slots)):
+        day[x] = 'free'
+    slot = random.randint(0,halfslot)
+    if slot == 3:
+            return newHalfDay()
     day[slot] = 'prac'
     day[slot + 1] = 'prac'
     return day
 
 
+def isDupSubjectPresent(day):
+    for subject in subjects:
+        count = 0
+        for daySub in day:
+            if subject == daySub:
+                count = count + 1
+        if count > 1:
+            return subject
+    return False
+            
+
 def isSubAssignable(day,sub):
+    if isDupSubjectPresent(day):
+        maxCount = 1
+    else:
+        maxCount = 2
     count = 0
     for x in day:
         if x == sub:
             count = count + 1
-    if count < 2:
+    if count < maxCount:
         return True
     else:
         return False
@@ -41,10 +70,44 @@ def assignSlot(day,sub):
         day[slot] = sub
         return day
 
-def newDay():
-    day = newEmptyDay()
-    for x in range(slots-2):
+def newDay(halfDay = False):
+    random.seed()
+    if halfDay:
+        day = newHalfDay()
+        lecs = halfslot - 2
+    else:
+        day = newEmptyDay()
+        lecs = slots-2
+    for x in range(lecs):
         day = assignSlot(day,getAssignableSub(day))
     return day
 
-newDay()
+def isDayValid(week,day):
+    subjectCount = {'prac':0, 'free':0}
+    for x in subjects:
+        subjectCount[x] = 0
+    tempWeek = []
+    for weekday in week:
+        tempWeek.append(weekday)
+    tempWeek.append(day)
+    for weekday in tempWeek:
+        for subject in weekday:
+            subjectCount[subject] = subjectCount[subject] + 1
+    for subject in subjectCount:
+        if not subject == 'prac' and not subject == 'free':
+            if subjectCount[subject] > maxSubWeek:
+                return False
+    return True
+
+def newTimeTable():
+    week = []
+    while len(week) < daysInWeek:
+        if(len(week) == 1):
+            day = newDay(True)
+        else:
+            day = newDay()
+        if isDayValid(week,day):
+            week.append(day)
+    return week
+
+print(newTimeTable())
